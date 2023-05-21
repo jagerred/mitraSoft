14ddb8a7f84fd39ac2af99f2623b863478552e29
Vlad Gritsavka <griczavka2363@gmail.com>
2023-05-21 15:35:43 +0300

    исправил класс lazyLoader

--- src/components/LazyLoader/LazyLoader.jsx
+++ src/components/LazyLoader/LazyLoader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Spinner } from 'react-bootstrap';
 
 const LazyLoader = () => (
-	<Container className='lazy-loader'>
+	<Container className='lazyLoader'>
 		<Spinner animation='border' role='status'>
 			<span className='visually-hidden'>Loading...</span>
 		</Spinner>
