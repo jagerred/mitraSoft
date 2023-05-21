3a8500d564dff617e002ebd4af52cbcb1414b733
Vlad Gritsavka <griczavka2363@gmail.com>
2023-05-21 15:32:01 +0300

    добавил кнопку "назад"

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import { Container } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { Button, Container } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
 import Posts from '../../components/Posts/Posts';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPosts } from '../../redux/slices/posts';
@@ -8,12 +8,17 @@ import { getPosts } from '../../redux/slices/posts';
 const UserPage = () => {
 	const { posts } = useSelector(state => state.posts);
 	const { userId } = useParams();
+	const navigate = useNavigate();
 	const dispatch = useDispatch();
 	useEffect(() => {
 		dispatch(getPosts(userId));
 	}, []);
+	const goBack = () => navigate(-1);
 	return (
 		<Container className='p-0'>
+			<Button className='mb-2' onClick={goBack}>
+				Назад
+			</Button>
 			<Container className='userInfo'>
 				<h3>{`Пользователь ${userId}`}</h3>
 				<span>{`Всего ${posts.length} постов`}</span>
